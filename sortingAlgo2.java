80123239f4299979bf9e2c1069c321481005e878
himanshubuyerteam <himanshu.mh@udaan.com>
2026-04-12 13:01:14 +0530

    insertion

--- sortingAlgo2.java
+++ sortingAlgo2.java
@@ -96,6 +96,21 @@ public class sortingAlgo2 {
     {
         return mergeSortHelper(arr, 0,arr.length-1);
     }
+
+    public void insertionSort(int []arr)
+    {
+        int n=arr.length;
+        for(int i=1;i<n;i++)
+        {
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
     public static void main(String[] args) {
         int []a1={1,4,7,9,19,22,25,29};
         int []a2={2,3};
